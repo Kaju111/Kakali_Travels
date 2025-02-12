7f8983ac7254ee853f7268c56681bf208e551236
deviid123 <devidkoch321@gmail.com>
2025-02-12 14:10:41 +0000

    changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import Facilities from "./components/Facilities";
 import Gallery from "./components/Gallery";
 import HeroSection from "./components/Home";
 import LocalExperiences from "./components/LocalExperiences";
-import Testimonial from "./components/Testimonial";
+// import Testimonial from "./components/Testimonial";
 import Unformation from "./components/Unformation";
 import PackageSearch from "../../components/packages/PackageSearch";
 
@@ -15,7 +15,7 @@ const Home = () => {
             <LocalExperiences />
             <Facilities />
             <Gallery />
-            <Testimonial />
+            {/* <Testimonial /> */}
             <Unformation />
         </div>
     );
